8c9a2282159f31031713b1544eee1b49f95398ef
Mohit101192 <Mohit101192@github.com>
2021-03-01 21:53:16 +0530

    Added

---
+++ serviceprovider/src/main/java/com/nagarro/msa/service/provider/ServiceproviderApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.msa.service.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceproviderApplication.class, args);
+	}
+
+}
---
+++ serviceprovider/src/main/java/com/nagarro/msa/service/provider/service/ServiceProviderServiceIMPL.java
@@ -0,0 +1,87 @@
+package com.nagarro.msa.service.provider.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import com.nagarro.msa.service.provider.config.OrderClient;
+import com.nagarro.msa.service.provider.config.UserClient;
+import com.nagarro.msa.service.provider.model.ServiceProviderUser;
+import com.nagarro.msa.service.provider.model.User;
+
+
+public class ServiceProviderServiceIMPL implements ServiceProviderService{
+
+	@Autowired
+	private UserClient userClient;
+	
+	@Autowired
+	private OrderClient orderClient;
+	
+	@Autowired
+	@Qualifier("spOrderList")
+	private Map<String, List<Integer>> spOrderList;
+	
+	@Override
+	public String assignSp(String username, int orderId) {
+		if(spOrderList.get(username)==null)
+		{
+			List<Integer> orderList = new ArrayList<>();
+			spOrderList.put(username, orderList);
+		}
+		else
+		{
+		
+			List<Integer> orderList = spOrderList.get(username);
+			orderList.add(orderId);
+			
+		}
+		return orderClient.spAssigned(orderId);
+	}
+
+	@Override
+	public List<ServiceProviderUser> spList(int cityId) {
+		List<User> userList = userClient.spList(cityId);
+		List<ServiceProviderUser> spList = new ArrayList<>();
+		for(User user: userList)
+		{
+			ServiceProviderUser spUser = new ServiceProviderUser();
+			spUser.setAddress(user.getShopAddress());
+			spUser.setCityId(user.getCityId());
+			spUser.setUsername(user.getUsername());
+			spUser.setPhone(user.getMobile());
+		}
+
+	return spList;
+	}
+
+	@Override
+	public List<Integer> fetchOrders(String username) {
+		return spOrderList.get(username);
+	}
+
+	@Override
+	public String updateOrder(String username, int orderId) {
+		if(spOrderList.get(username)!=null)
+		{
+			List<Integer> orderList = spOrderList.get(username);
+			int loc =0;
+			for(int i:orderList)
+			{
+				if(i==orderId)
+				{
+					break;
+				}
+				loc++;
+			}
+			orderList.remove(loc);
+		}
+
+		return orderClient.orderCompleted(orderId);
+	}
+
+
+}
